02fac31592ee0c6719eb2f915e2035ce8d02251e
jisoo7336 <93235120+jisoo7336@users.noreply.github.com>
2022-01-19 17:37:01 +0900

    2022.01.19

---
+++ js/header.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#b").click(function(){
+     $("#y").slideToggle();
+    });
+});
+   
+   
+
+$(window).resize(function(){
+    if(window.innerWidth<=1099){
+        $('#y').hide();
+    }
+});
+
